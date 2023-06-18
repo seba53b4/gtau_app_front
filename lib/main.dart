9116a8b79ad63f8fecb23ae4367d0e0f45b6cf89
seba53b4 <seba5364@hotmail.com>
2023-06-17 21:12:06 -0300

    Global_Creation- GOOGLE_API_KEY logic to web added

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,12 @@ import 'package:gtau_app_front/providers/user_provider.dart';
 import 'package:gtau_app_front/screens/LoginScreen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:js' as js;
+import 'dart:html' as html;
 
 Future<void> main() async {
   await dotenv.load();
-
+  html.document.dispatchEvent(html.CustomEvent("google-maps-api-key-loaded", detail: {"GOOGLE_API_KEY": dotenv.env['GOOGLE_API_KEY'] }));
   runApp(
     MultiProvider(
       providers: [
