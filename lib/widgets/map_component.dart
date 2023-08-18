64d5bcec6831d1de55ab8d1299b1eaddb94269d1
seba53b4 <seba5364@hotmail.com>
2023-08-17 21:20:18 -0300

    feature-25: Delete unused import

--- lib/widgets/map_component.dart
+++ lib/widgets/map_component.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:gtau_app_front/viewmodels/section_viewmodel.dart';
-import 'package:gtau_app_front/widgets/sectionDetail.dart';
 import 'package:provider/provider.dart';
 
 import '../models/section_data.dart';
