2940046228e7711046f76affdffb76ca7cbb8f77
seba53b4 <seba5364@hotmail.com>
2023-08-19 00:42:07 -0300

    Feature-40: Added location button manual, get location of device & distance control button

--- lib/widgets/map_component.dart
+++ lib/widgets/map_component.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -13,19 +15,28 @@ class MapComponent extends StatefulWidget {
 }
 
 class _MapComponentState extends State<MapComponent> {
-  Position? location;
+  LatLng? location;
+  static const LatLng initLocation = LatLng(-34.88773, -56.13955);
   String? errorMsg;
+  List<String> distances = ["100", "200", "300", "500"];
+  int distanceSelected = 0;
   MapType _currentMapType = MapType.satellite;
   Set<Polyline> polylines = {};
+  Set<Marker> markers = {};
   bool isSectionDetailsVisible = false;
   PolylineId selectedPolylineId = PolylineId('');
   Color selectedPolylineColor = Colors.greenAccent;
   Color defaultPolylineColor = Colors.redAccent;
+  Color selectedButtonColor = Colors.green;
+  Color defaultButtonColor = Colors.primaries.first;
+  bool locationManual = false;
+  late Completer<GoogleMapController> _mapController;
 
   @override
   void initState() {
     super.initState();
     getCurrentLocation();
+    _mapController = Completer();
   }
 
   Future<void> getCurrentLocation() async {
@@ -41,8 +52,20 @@ class _MapComponentState extends State<MapComponent> {
 
       Position currentPosition = await Geolocator.getCurrentPosition();
       setState(() {
-        location = currentPosition;
+        location = LatLng(currentPosition.latitude, currentPosition.longitude);
       });
+
+      // Actualiza la cámara del mapa para centrarse en la ubicación actual
+      final GoogleMapController controller = await _mapController.future;
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: LatLng(currentPosition.latitude, currentPosition.longitude),
+            zoom: 15,
+          ),
+        ),
+      );
+
     } catch (e) {
       setState(() {
         errorMsg = 'Error fetching location';
@@ -53,7 +76,13 @@ class _MapComponentState extends State<MapComponent> {
 
   Future<List<Section>?> fetchPolylines(String token) async{
     final sectionViewModel = Provider.of<SectionViewModel>(context, listen: false);
-    List<Section>? sections = await sectionViewModel.fetchSectionsByRadius(token, -34.88648, -56.13854, 500);
+    List<Section>? sections;
+    if (location != null){
+      sections = await sectionViewModel.fetchSectionsByRadius(token, location!.latitude, location!.longitude, int.parse(distances[distanceSelected]));
+    } else {
+      sections = await sectionViewModel.fetchSectionsByRadius(token, initLocation.latitude, initLocation.longitude,  int.parse(distances[distanceSelected]));
+    }
+
     return sections;
   }
 
@@ -83,16 +112,6 @@ class _MapComponentState extends State<MapComponent> {
     }
   }
 
-  void handlePolylinePress(String id) {
-    showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Text('Línea clickeada'),
-        content: Text('Línea con ID: $id fue clickeada'),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final token = context.read<UserProvider>().getToken;
@@ -103,24 +122,30 @@ class _MapComponentState extends State<MapComponent> {
           GoogleMap(
             mapType: _currentMapType,
             initialCameraPosition: const CameraPosition(
-              target: LatLng(-34.88773, -56.13955),
+              target: initLocation,
               zoom: 15,
             ),
             polylines: polylines,
+            markers: markers,
             myLocationEnabled: true,
             myLocationButtonEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _mapController.complete(controller);
+            },
+            onTap: (LatLng latLng) {
+              if (locationManual) {
+                setState(() {
+                  final Marker newMarker = Marker(
+                    markerId: const MarkerId('tapped_location'),
+                    position: latLng,
+                  );
+
+                  markers.add(newMarker);
+                  location = LatLng(latLng.latitude, latLng.longitude);
+                });
+              }
+            },
           ),
-          // GestureDetector(
-          //   onTap: () {
-          //   },
-          //   child: AbsorbPointer(
-          //     absorbing: isSectionDetailsVisible,
-          //     child: MapSectionDetailsOverlay(
-          //       isVisible: isSectionDetailsVisible,
-          //       selectedPolylineId: selectedPolylineId,
-          //     ),
-          //   ),
-          // ),
           Positioned(
             bottom: 14,
             left: 16,
@@ -151,7 +176,32 @@ class _MapComponentState extends State<MapComponent> {
                     });
                   },
                   child: Text("Fetch Tramos"),
-                )
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    getCurrentLocation();
+                  },
+                  child: Text("Obtener Ubicación"),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: (locationManual) ? selectedButtonColor : defaultButtonColor ,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      locationManual = !locationManual;
+                    });
+                  },
+                  child: Text("Seleccionar Ubicación"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      distanceSelected = (distanceSelected + 1) % distances.length;
+                    });
+                  },
+                  child: Text(distances[distanceSelected].toString()),
+                ),
               ],
             ),
           ),
