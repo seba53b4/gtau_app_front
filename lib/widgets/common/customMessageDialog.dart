32b7b38791fe1630edd2daa2796fa325cbc19787
seba53b4 <seba5364@hotmail.com>
2023-11-10 21:36:36 -0300

    fix on dialogs

--- lib/widgets/common/customMessageDialog.dart
+++ lib/widgets/common/customMessageDialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:gtau_app_front/constants/theme_constants.dart';
 
 enum DialogMessageType {
   success,
@@ -15,12 +16,12 @@ extension DialogMessageTypeExtension on DialogMessageType {
       case DialogMessageType.error:
         return 'Error';
       case DialogMessageType.warning:
-        return 'Advertencia!';
+        return 'Advertencia';
     }
   }
 }
 
-class MessageDialog extends StatelessWidget {
+class MessageDialog extends StatefulWidget {
   final DialogMessageType messageType;
   final VoidCallback onAcceptPressed;
 
@@ -30,15 +31,44 @@ class MessageDialog extends StatelessWidget {
   });
 
   @override
+  _MessageDialogState createState() => _MessageDialogState();
+}
+
+class _MessageDialogState extends State<MessageDialog>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     IconData iconData;
     Color iconColor;
     String textContent;
 
-    switch (messageType) {
+    switch (widget.messageType) {
       case DialogMessageType.success:
         iconData = Icons.check_circle;
-        iconColor = Colors.green;
+        iconColor = primarySwatch;
         textContent = DialogMessageType.success.value;
         break;
       case DialogMessageType.error:
@@ -56,21 +86,67 @@ class MessageDialog extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Row(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(iconData, color: iconColor),
-            const SizedBox(width: 10),
-            Text(textContent),
+            Text(
+              textContent,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Divider(
+              height: 8,
+              thickness: 2,
+              color: Colors.grey.shade100,
+            ),
+            const SizedBox(height: 12),
+            AnimatedBuilder(
+              animation: _scaleAnimation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _scaleAnimation.value,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey[200],
+                    ),
+                    padding: const EdgeInsets.all(24),
+                    child: Icon(iconData, color: iconColor, size: 62),
+                  ),
+                );
+              },
+            ),
           ],
         ),
-        const SizedBox(height: 10),
-        ElevatedButton(
-          child: Text(AppLocalizations.of(context)!.buttonAcceptLabel),
+        const SizedBox(height: 12),
+        Divider(
+          height: 8,
+          thickness: 2,
+          color: Colors.grey.shade100,
+        ),
+        const SizedBox(height: 8),
+        TextButton(
+          style: TextButton.styleFrom(
+            textStyle: Theme.of(context).textTheme.labelLarge,
+          ),
           onPressed: () {
             Navigator.of(context).pop();
-            onAcceptPressed();
+            widget.onAcceptPressed();
           },
+          child: Text(AppLocalizations.of(context)!.dialogCloseButton),
         ),
+        // CustomElevatedButton(
+        //   width: 60,
+        //   height: 60,
+        //   text: AppLocalizations.of(context)!.dialogCloseButton,
+        //   onPressed: () {
+        //     Navigator.of(context).pop();
+        //     widget.onAcceptPressed();
+        //   },
+        // ),
       ],
     );
   }
@@ -88,6 +164,9 @@ Future<void> showCustomMessageDialog({
         content: MessageDialog(
           messageType: messageType,
           onAcceptPressed: onAcceptPressed,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
         ),
       );
     },
