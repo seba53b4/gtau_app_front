9d800a0c379776caa63bfbbb072fe2e5fd893b5d
seba53b4 <seba5364@hotmail.com>
2023-12-04 22:43:23 -0300

    merge with dev

--- lib/screens/TaskCreationScreen.dart
+++ lib/screens/TaskCreationScreen.dart
@@ -27,11 +27,10 @@ import '../widgets/common/custom_dropdown.dart';
 import '../widgets/common/custom_elevated_button.dart';
 import '../widgets/common/custom_text_form_field.dart';
 import '../widgets/common/custom_toggle_buttons.dart';
+import '../widgets/common/inspection_location_select.dart';
 import '../widgets/common/task_creation/create_scheduled.dart';
 import '../widgets/common/task_creation/element_selected.dart';
 import '../widgets/image_gallery_modal.dart';
-import '../widgets/map_modal.dart';
-import '../widgets/map_modal_location_select.dart';
 import '../widgets/user_image.dart';
 
 class TaskCreationScreen extends StatefulWidget {
@@ -1318,82 +1317,6 @@ class _TaskCreationScreenState extends State<TaskCreationScreen> {
             ),
           ),
         ),
-      );
-    });
-  }
-}
-
-class InspectionLocationSelect extends StatelessWidget {
-  const InspectionLocationSelect({
-    super.key,
-    required this.selectedItemsProvider,
-  });
-
-  final SelectedItemsProvider? selectedItemsProvider;
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<SelectedItemsProvider>(
-        builder: (context, selectedItemsProvider, child) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            AppLocalizations.of(context)!.createTaskPage_selectUbicationTitle,
-            style: const TextStyle(
-              fontSize: 18,
-            ),
-          ),
-          const SizedBox(height: 12),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const MapModalLocationSelect(),
-              const SizedBox(width: 10.0),
-              Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: softGrey,
-                  borderRadius: BorderRadius.circular(24.0),
-                ),
-                child: Wrap(
-                  direction: Axis.vertical,
-                  spacing: 8.0,
-                  runSpacing: 15.0,
-                  children: [
-                    Chip(
-                      backgroundColor: Colors.white70,
-                      avatar: CircleAvatar(
-                        backgroundColor: Colors.black38,
-                        child: Icon(
-                          Icons.location_on_outlined,
-                          color: Colors.white70.withOpacity(1),
-                          size: 20,
-                        ),
-                      ),
-                      label: Text(
-                          "lat: ${selectedItemsProvider.inspectionPosition.latitude}"),
-                    ),
-                    Chip(
-                      backgroundColor: Colors.white70,
-                      avatar: CircleAvatar(
-                        backgroundColor: Colors.black38,
-                        child: Icon(
-                          Icons.location_on_outlined,
-                          color: Colors.white70.withOpacity(1),
-                          size: 20,
-                        ),
-                      ),
-                      label: Text(
-                          " long: ${selectedItemsProvider.inspectionPosition.longitude}"),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ],
       );
     });
   }
