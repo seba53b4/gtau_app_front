f3b3251c8c6a24cbaab93e1bd4557e5de2d85722
seba53b4 <seba5364@hotmail.com>
2024-02-12 21:48:38 -0300

    bugfix: profile size on web

--- lib/screens/ProfileScreen.dart
+++ lib/screens/ProfileScreen.dart
@@ -95,7 +95,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
             backgroundColor: lightBackground,
             body: Center(
               child: SizedBox(
-                width: kIsWeb ? widthCard * 0.3 : widthCard,
+                width: kIsWeb ? 520 : widthCard,
                 height: kIsWeb ? heightCard * 0.85 : heightCard,
                 child: Card(
                   child: Visibility(
@@ -106,7 +106,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       children: [
                         Container(
                           height: 250,
-                          width: kIsWeb ? widthCard * 0.3 : widthCard,
+                          width: kIsWeb ? 520 : widthCard,
                           padding: const EdgeInsets.only(top: 24, bottom: 24),
                           decoration: BoxDecoration(
                             color: primarySwatch[200],
